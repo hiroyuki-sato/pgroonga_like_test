645b8adfa231019ab923e5b59732e593ffaadabe
Hiroyuki Sato <hiroysato@gmail.com>
2015-12-24 17:16:16 +0900

    add q7 query

---
+++ q7.sql
@@ -0,0 +1,23 @@
+SET enable_seqscan = off;
+SET enable_indexscan = on;
+SET enable_bitmapscan = on;
+
+set pgroonga.log_level = 'debug';
+
+set work_mem = '100MB';
+
+EXPLAIN SELECT
+  u.url 
+FROM 
+  url_lists u,
+  keywords k
+WHERE
+  u.url like k.url;
+
+SELECT
+  u.url 
+FROM 
+  url_lists u,
+  keywords k
+WHERE
+  u.url like k.url;
