ab61ddbbe511917b3d38b5b02c0fc9fa4bc98d7d
Hiroyuki Sato <hiroysato@gmail.com>
2015-12-26 12:15:29 +0900

    Add new sample generator

---
+++ sample_gen2.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby 
+require 'csv'
+require 'securerandom'
+require 'uri'
+
+
+idx = 0
+('a'..'z').each do |u_char|
+  ('aaa'..'zzz').each do |p_str|
+    1.upto(50).each do |n|
+      idx += 1
+      puts [idx,"http://a#{u_char}.yahoo.co.jp/"+p_str+SecureRandom.hex(15)].to_csv
+    end
+  end
+
+end
